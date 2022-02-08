b8ced423af532fbc9fd8d3a4e6ff78c6258de642
Keonil <69555662+hehe1004@users.noreply.github.com>
2022-02-08 18:01:53 +0900

    도메인 개발

---
+++ src/main/java/hello/itemservice/doamin/item/Item.java
@@ -0,0 +1,27 @@
+package hello.itemservice.doamin.item;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Data
+//@Getter @Setter
+public class Item {
+
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private Integer quantity;
+
+
+
+    public Item() {
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
